1157ac6518c93601984d97abf458ebe144c80acc
David Ellis <isv.damocles@gmail.com>
2019-02-28 14:07:43 -0800

    Attempt to adjust the build steps to work for Windows

--- .install.sh
+++ .install.sh
@@ -17,28 +17,38 @@ pushd h3c
 git pull origin master --tags
 git checkout "$VERSION"
 
-# Run CMake, installing a recent version if not found or not compatible
-{
-	cmake -DENABLE_FORMAT=OFF -DBUILD_SHARED_LIBS=ON .
-} || {
-	# Install modern CMake
-	mkdir cmake-download
-	pushd cmake-download
-	curl -O https://cmake.org/files/v3.10/cmake-3.10.0-rc5-Linux-x86_64.sh
-	bash cmake-3.10.0-rc5-Linux-x86_64.sh --skip-license
-	export PATH=`pwd`/bin:$PATH
-	echo $PATH
-	popd
-	cmake -DENABLE_FORMAT=OFF -DBUILD_SHARED_LIBS=ON .
-}
+if command -v make; then
+  # Run CMake, installing a recent version if not found or not compatible
+  {
+    cmake -DENABLE_FORMAT=OFF -DBUILD_SHARED_LIBS=ON .
+  } || {
+    # Install modern CMake
+    mkdir cmake-download
+    pushd cmake-download
+    curl -O https://cmake.org/files/v3.10/cmake-3.10.0-rc5-Linux-x86_64.sh
+    bash cmake-3.10.0-rc5-Linux-x86_64.sh --skip-license
+    export PATH=`pwd`/bin:$PATH
+    echo $PATH
+    popd
+    cmake -DENABLE_FORMAT=OFF -DBUILD_SHARED_LIBS=ON .
+  }
 
-make
-ls -l lib/libh3*
-cp lib/libh3* ../h3/out
-if [ -e ../build ] && [ -d ../build ]; then
-    LIBNAME=`ls ../build/ | grep '^lib'`
-    mkdir -p ../build/$LIBNAME/h3/out
-    cp lib/libh3* ../build/$LIBNAME/h3/out
+  make
+  ls -l lib/libh3*
+  cp lib/libh3* ../h3/out
+  if [ -e ../build ] && [ -d ../build ]; then
+      LIBNAME=`ls ../build/ | grep '^lib'`
+      mkdir -p ../build/$LIBNAME/h3/out
+      cp lib/libh3* ../build/$LIBNAME/h3/out
+  fi
+else
+  if [[ "${Platform}" == "x64" ]]; then
+    cmake . -DCMAKE_VS_PLATFORM_NAME=${Platform} -G "Visual Studio 14 Win64" && msbuild.exe h3.vcxproj;
+  else
+    cmake . -DCMAKE_VS_PLATFORM_NAME=${Platform} && msbuild.exe h3.vcxproj;
+  fi
+  ls -l bin/Release/h3*
+  cp bin/Release/h3.lib ../h3/out
 fi
 popd
 rm -rf h3c
