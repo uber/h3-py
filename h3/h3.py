e539a321029a2a06c18d6794823dca28d28cdfdb
David Ellis <isv.damocles@gmail.com>
2019-02-28 16:25:55 -0800

    Fix copy-paste issue

--- h3/h3.py
+++ h3/h3.py
@@ -31,6 +31,7 @@ from ctypes import (
     byref,
     Structure,
     POINTER,
+    util as ctypes_util,
 )
 
 _dirname = os.path.dirname(__file__)
@@ -41,7 +42,7 @@ libh3_path = ('{}/{}'.format(_dirname, 'out/libh3.1.dylib')
 
 # Load the Windows version of libc for H3 to use (calloc, free, etc)
 if platform.system() == 'Windows':
-    ctypes.CDLL(ctypes.util.find_msvcrt())
+    cdll(ctypes_util.find_msvcrt())
 
 libh3 = cdll.LoadLibrary(libh3_path)
 
