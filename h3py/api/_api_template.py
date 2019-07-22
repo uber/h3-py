a352d7cd45eb4a3d3c1fc4c0eb1eb44ab4ecf139
AJ Friend <ajfriend@uber.com>
2019-07-22 13:34:49 -0700

    rename functions to match current python implementation

--- h3py/api/_api_template.py
+++ h3py/api/_api_template.py
@@ -15,10 +15,10 @@ def _api_functions(
     _in_collection,
     _out_collection,
 ):
-    def hex2int(h):
+    def string_to_h3(h):
         return u.hex2int(h)
 
-    def int2hex(x):
+    def h3_to_string(x):
         return u.int2hex(x)
 
     def num_hexagons(resolution):
@@ -30,7 +30,7 @@ def _api_functions(
     def mean_edge_length(resolution, unit='km'):
         return h3core.mean_edge_length(resolution, unit)
 
-    def is_cell(h):
+    def h3_is_valid(h):
         """Validates an H3 cell (hexagon or pentagon)
 
         Returns
@@ -43,7 +43,7 @@ def _api_functions(
         except:
             return False
 
-    def is_edge(edge):
+    def h3_unidirectional_edge_is_valid(edge):
         try:
             e = _in_scalar(edge)
             return h3core.is_edge(e)
@@ -59,14 +59,14 @@ def _api_functions(
         """Reverse lookup an h3 address into a geo-coordinate"""
         return h3core.h3_to_geo(_in_scalar(h))
 
-    def resolution(h):
+    def h3_get_resolution(h):
         """Returns the resolution of an `h3_address`
 
         :return: nibble (0-15)
         """
         return h3core.resolution(_in_scalar(h))
 
-    def parent(h, res=None):
+    def h3_to_parent(h, res=None):
         """ Get the parent of a hexagon.
 
         Parameters
@@ -87,7 +87,7 @@ def _api_functions(
 
         return p
 
-    def distance(h1, h2):
+    def h3_distance(h1, h2):
         """ Compute the hex-distance between two hexagons
 
         Parameters
@@ -106,21 +106,21 @@ def _api_functions(
 
         return d
 
-    def cell_boundary(h, geo_json=False):
+    def h3_to_geo_boundary(h, geo_json=False):
         return h3core.cell_boundary(_in_scalar(h), geo_json)
 
 
-    def disk(h, k=1):
+    def k_ring(h, k=1):
         mv = h3core.disk(_in_scalar(h), k)
 
         return _out_collection(mv)
 
-    def ring(h, k=1):
+    def hex_ring(h, k=1):
         mv = h3core.ring(_in_scalar(h), k)
 
         return _out_collection(mv)
 
-    def children(h, res=None):
+    def h3_to_children(h, res=None):
         """ Get the children of a hexagon.
 
         Parameters
@@ -157,7 +157,7 @@ def _api_functions(
 
         return _out_collection(mv)
 
-    def is_pentagon(h):
+    def h3_is_pentagon(h):
         """
         a pentagon should still pass is_cell(), right?
 
@@ -165,19 +165,19 @@ def _api_functions(
         """
         return h3core.is_pentagon(_in_scalar(h))
 
-    def get_base_cell(h):
+    def h3_get_base_cell(h):
         """
         :returns: boolean
         """
         return h3core.get_base_cell(_in_scalar(h))
 
-    def are_neighbors(h1, h2):
+    def h3_indexes_are_neighbors(h1, h2):
         """
         :returns: boolean
         """
         return h3core.are_neighbors(_in_scalar(h1), _in_scalar(h2))
 
-    def edge(origin, destination):
+    def get_h3_unidirectional_edge(origin, destination):
         o = _in_scalar(origin)
         d = _in_scalar(destination)
         e = h3core.edge(o, d)
@@ -185,14 +185,14 @@ def _api_functions(
 
         return e
 
-    def edge_origin(e):
+    def get_origin_h3_index_from_unidirectional_edge(e):
         e = _in_scalar(e)
         o = h3core.edge_origin(e)
         o = _out_scalar(o)
 
         return o
 
-    def edge_destination(e):
+    def get_destination_h3_index_from_unidirectional_edge(e):
         e = _in_scalar(e)
         d = h3core.edge_destination(e)
         d = _out_scalar(d)
@@ -200,22 +200,22 @@ def _api_functions(
         return d
 
 
-    def edge_cells(e):
+    def get_h3_indexes_from_unidirectional_edge(e):
         e = _in_scalar(e)
         o,d = h3core.edge_cells(e)
         o,d = _out_scalar(o), _out_scalar(d)
 
         return o,d
 
-    def edges_from_cell(origin):
+    def get_h3_unidirectional_edges_from_hexagon(origin):
         mv = h3core.edges_from_cell(_in_scalar(origin))
 
         return _out_collection(mv)
 
-    def edge_boundary(edge):
+    def get_h3_unidirectional_edge_boundary(edge):
         return h3core.edge_boundary(_in_scalar(edge))
 
-    def line(start, end):
+    def h3_line(start, end):
         mv = h3core.line(_in_scalar(start), _in_scalar(end))
 
         return _out_collection(mv)
--- tests/test.py
+++ tests/test.py
@@ -29,7 +29,7 @@ def test3():
         (37.775019673792606, -122.4195306280734)
      )
 
-    out = h3.cell_boundary('8928308280fffff')
+    out = h3.h3_to_geo_boundary('8928308280fffff')
     assert approx2(out, expected)
 
 
@@ -44,7 +44,7 @@ def test4():
         (-122.41719971841658, 37.775197782893386)
     )
 
-    out = h3.cell_boundary('8928308280fffff', geo_json=True)
+    out = h3.h3_to_geo_boundary('8928308280fffff', geo_json=True)
     assert approx2(out, expected)
 
 
@@ -59,13 +59,13 @@ def test5():
         '89283082803ffff'
     }
 
-    out = h3.disk('8928308280fffff', 1)
+    out = h3.k_ring('8928308280fffff', 1)
     assert out == expected
 
 
 def test6():
     expected = {'8928308280fffff'}
-    out = h3.ring('8928308280fffff', 0)
+    out = h3.hex_ring('8928308280fffff', 0)
     assert out == expected
 
 def test7():
@@ -78,29 +78,29 @@ def test7():
         '89283082877ffff'
     }
 
-    out = h3.ring('8928308280fffff', 1)
+    out = h3.hex_ring('8928308280fffff', 1)
     assert out == expected
 
 def test8():
-    assert h3.is_cell('89283082803ffff')
-    assert not h3.is_cell('abc')
+    assert h3.h3_is_valid('89283082803ffff')
+    assert not h3.h3_is_valid('abc')
 
     # looks like it might be valid, but it isn't!
     h_bad = '8a28308280fffff'
-    assert not h3.is_cell(h_bad)
+    assert not h3.h3_is_valid(h_bad)
 
     # other methods should validate and raise exception if bad input
     with pytest.raises(InvalidH3Address):
-        h3.resolution(h_bad)
+        h3.h3_get_resolution(h_bad)
 
 def test9():
-    assert h3.resolution('8928308280fffff') == 9
-    assert h3.resolution('8a28308280f7fff') == 10
+    assert h3.h3_get_resolution('8928308280fffff') == 9
+    assert h3.h3_get_resolution('8a28308280f7fff') == 10
 
 def test_parent():
-    assert h3.parent('8928308280fffff', 8) == '8828308281fffff'
-    assert h3.parent('8928308280fffff', 7) == '872830828ffffff'
-    assert h3.parent('8928308280fffff', 10) == '0' # todo: thsi should probably return None, eh?
+    assert h3.h3_to_parent('8928308280fffff', 8) == '8828308281fffff'
+    assert h3.h3_to_parent('8928308280fffff', 7) == '872830828ffffff'
+    assert h3.h3_to_parent('8928308280fffff', 10) == '0' # todo: thsi should probably return None, eh?
 
 
 def test_children():
@@ -114,24 +114,24 @@ def test_children():
         '8a28308280f7fff'
     }
 
-    out = h3.children('8928308280fffff', 10)
+    out = h3.h3_to_children('8928308280fffff', 10)
 
     assert out == expected
 
     expected = set()
-    out = h3.children('8928308280fffff', 8)
+    out = h3.h3_to_children('8928308280fffff', 8)
 
     assert out == expected
 
 def test_distance():
     h = '8a28308280c7fff'
-    assert h3.distance(h,h) == 0
+    assert h3.h3_distance(h,h) == 0
 
-    n = h3.ring(h,1).pop()
-    assert h3.distance(h,n) == 1
+    n = h3.hex_ring(h,1).pop()
+    assert h3.h3_distance(h,n) == 1
 
-    n = h3.ring(h,2).pop()
-    assert h3.distance(h,n) == 2
+    n = h3.hex_ring(h,2).pop()
+    assert h3.h3_distance(h,n) == 2
 
 
 def test_polyfill():
@@ -274,39 +274,39 @@ def test_edge():
     h1 = '8928308280fffff'
     h2 = '89283082873ffff'
 
-    assert not h3.are_neighbors(h1, h1)
-    assert h3.are_neighbors(h1, h2)
+    assert not h3.h3_indexes_are_neighbors(h1, h1)
+    assert h3.h3_indexes_are_neighbors(h1, h2)
 
-    e = h3.edge(h1,h2)
+    e = h3.get_h3_unidirectional_edge(h1,h2)
 
     assert e == '12928308280fffff'
-    assert h3.is_edge(e)
-    assert not h3.is_cell(e)
+    assert h3.h3_unidirectional_edge_is_valid(e)
+    assert not h3.h3_is_valid(e)
 
-    assert h3.edge_origin(e) == h1
-    assert h3.edge_destination(e) == h2
+    assert h3.get_origin_h3_index_from_unidirectional_edge(e) == h1
+    assert h3.get_destination_h3_index_from_unidirectional_edge(e) == h2
 
-    assert h3.edge_cells(e) == (h1,h2)
+    assert h3.get_h3_indexes_from_unidirectional_edge(e) == (h1,h2)
 
 def test_edges_from_cell():
     h = '8928308280fffff'
-    edges = h3.edges_from_cell(h)
-    destinations = {h3.edge_destination(e) for e in edges}
-    neighbors = h3.ring(h, 1)
+    edges = h3.get_h3_unidirectional_edges_from_hexagon(h)
+    destinations = {h3.get_destination_h3_index_from_unidirectional_edge(e) for e in edges}
+    neighbors = h3.hex_ring(h, 1)
 
     assert neighbors == destinations
 
 def test_edge_boundary():
     h1 = '8928308280fffff'
     h2 = '89283082873ffff'
-    e = h3.edge(h1,h2)
+    e = h3.get_h3_unidirectional_edge(h1,h2)
 
     expected = (
         (37.77688044840226, -122.41612835779266),
         (37.778385004930925, -122.41738797617619)
     )
 
-    out = h3.edge_boundary(e)
+    out = h3.get_h3_unidirectional_edge_boundary(e)
 
     assert out[0] == pytest.approx(expected[0])
     assert out[1] == pytest.approx(expected[1])
@@ -316,25 +316,25 @@ def test_validation():
     h = '8a28308280fffff' # invalid hex
 
     with pytest.raises(InvalidH3Address):
-        h3.get_base_cell(h)
+        h3.h3_get_base_cell(h)
 
     with pytest.raises(InvalidH3Address):
-        h3.resolution(h)
+        h3.h3_get_resolution(h)
 
     with pytest.raises(InvalidH3Address):
-        h3.parent(h, 9)
+        h3.h3_to_parent(h, 9)
 
     with pytest.raises(InvalidH3Address):
-        h3.distance(h, h)
+        h3.h3_distance(h, h)
 
     with pytest.raises(InvalidH3Address):
-        h3.disk(h, 1)
+        h3.k_ring(h, 1)
 
     with pytest.raises(InvalidH3Address):
-        h3.ring(h, 1)
+        h3.hex_ring(h, 1)
 
     with pytest.raises(InvalidH3Address):
-        h3.children(h, 11)
+        h3.h3_to_children(h, 11)
 
     with pytest.raises(InvalidH3Address):
         h3.compact({h})
@@ -347,9 +347,9 @@ def test_validation2():
     h = '8928308280fffff'
 
     with pytest.raises(InvalidH3Resolution):
-        h3.children(h, 17)
+        h3.h3_to_children(h, 17)
 
-    assert not h3.are_neighbors(h,h)
+    assert not h3.h3_indexes_are_neighbors(h,h)
 
 
 
@@ -364,40 +364,40 @@ def test_validation_geo():
         h3.geo_to_h3(0,0,17)
 
     with pytest.raises(InvalidH3Address):
-        h3.cell_boundary(h)
+        h3.h3_to_geo_boundary(h)
 
     with pytest.raises(InvalidH3Address):
-        h3.are_neighbors(h,h)
+        h3.h3_indexes_are_neighbors(h,h)
 
 def test_edges():
     h = '8928308280fffff'
 
     with pytest.raises(H3ValueError):
-        h3.edge(h,h)
+        h3.get_h3_unidirectional_edge(h,h)
 
-    h2 = h3.ring(h,2).pop()
+    h2 = h3.hex_ring(h,2).pop()
     with pytest.raises(H3ValueError):
-        h3.edge(h, h2)
+        h3.get_h3_unidirectional_edge(h, h2)
 
 
     e_bad = '14928308280ffff1'
-    assert not h3.is_edge(e_bad)
+    assert not h3.h3_unidirectional_edge_is_valid(e_bad)
 
     with pytest.raises(InvalidH3Edge):
-        h3.edge_origin(e_bad)
+        h3.get_origin_h3_index_from_unidirectional_edge(e_bad)
 
     with pytest.raises(InvalidH3Edge):
-        h3.edge_destination(e_bad)
+        h3.get_destination_h3_index_from_unidirectional_edge(e_bad)
 
     with pytest.raises(InvalidH3Edge):
-        h3.edge_cells(e_bad)
+        h3.get_h3_indexes_from_unidirectional_edge(e_bad)
 
 
 def test_line():
     h1 = '8928308280fffff'
     h2 = '8928308287bffff'
 
-    out = h3.line(h1,h2)
+    out = h3.h3_line(h1,h2)
 
     expected = {
         '8928308280fffff',
