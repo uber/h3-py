8ac7a99019df799ad3c97f8b84de046734a4668c
Ankit Mehta <ankitmehta@uber.com>
2019-09-18 15:34:29 -0700

    Change directory structure, make tests work

---
+++ src/h3/__init__.py
@@ -0,0 +1,2 @@
+from h3.api.set_str import *   # noqa
+from h3._version import __version__   # noqa
--- src/h3/api/array_int.py
+++ src/h3/api/array_int.py
@@ -1,24 +1,28 @@
-from h3py.api._api_template import _api_functions
+from h3.api._api_template import _api_functions
+from h3.h3utils import from_iter
+
 
 # todo: how to write documentation once and have it carry over to each interface?
 
 def _id(x):
     return x
 
+
 def _in_collection(hexes):
     it = list(hexes)
 
     return from_iter(it)
+
 
 def _out_collection(mv):
     return set(mv)
 
 
 _funcs = _api_functions(
-    _in_scalar = _id,
-    _out_scalar = _id,
-    _in_collection = _in_collection,
-    _out_collection = _out_collection,
+    _in_scalar=_id,
+    _out_scalar=_id,
+    _in_collection=_in_collection,
+    _out_collection=_out_collection,
 )
 
 globals().update(_funcs)
