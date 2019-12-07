86000646b40719b1534e1436c754e7e838bf563a
AJ Friend <ajfriend@uber.com>
2019-12-07 14:24:46 -0800

    use relative imports

--- src/h3/__init__.py
+++ src/h3/__init__.py
@@ -1,3 +1,5 @@
-from h3.api.set_str import *   # noqa
-import h3.api.set_str as h3  # noqa  # todo: add deprecation warning
-from h3._version import __version__   # noqa
+# flake8: noqa
+
+from .api.set_str import *
+from .api import set_str as h3
+from ._version import __version__
---
+++ src/h3/api/__init__.py
@@ -0,0 +1,3 @@
+from . import set_int
+from . import set_str
+from . import memview_int
