8b18867f9b86762b01b79b812e10d04bc3410487
AJ Friend <ajfriend@gmail.com>
2020-05-09 11:37:00 -0700

    adding long description to setup.py

--- setup.py
+++ setup.py
@@ -8,18 +8,29 @@ about = {}
 with open(os.path.join(here, 'src', 'h3', '_version.py')) as f:
     exec(f.read(), about)
 
+
+def long_desc():
+    here = os.path.abspath(os.path.dirname(__file__))
+    fname = os.path.join(here, 'readme.md')
+    with open(fname, encoding='utf-8') as f:
+        long_description = f.read()
+
+    return long_description
+
 setup(
     name = 'h3',
     version = about['__version__'],
     description = about['__description__'],
-    long_description = 'long_description',
+    long_description = long_desc(),
+    long_description_content_type = 'text/markdown',
+    license = about['__license__'],
     author = about['__author__'],
     author_email = about['__author_email__'],
     url = about['__url__'],
     classifiers = about['__classifiers__'],
     packages = find_packages(
         'src',
-        exclude=["*.tests", "*.tests.*", "tests.*", "tests"],
+        exclude = ["*.tests", "*.tests.*", "tests.*", "tests"],
     ),
     package_dir = {'': 'src'},
     cmake_languages = ('C'),
--- src/h3/_version.py
+++ src/h3/_version.py
@@ -1,10 +1,7 @@
 __version__ = '3.6.1'
-__description__ = (
-    'H3: A hierarchical hexagonal geospatial indexing system'
-    ' developed by Uber Technologies'
-)
+__description__ = 'Hierarchical hexagonal geospatial indexing system'
 __url__ = 'https://github.com/uber/h3-py'
-
+__license__ = "Apache 2.0 License"
 __author__ = 'Uber Technologies'
 __author_email__ = 'AJ Friend <ajfriend@gmail.com>'
 __classifiers__ = [
