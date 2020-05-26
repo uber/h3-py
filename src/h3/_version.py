08daf33b32388a779692f568d6f1758a2cc46247
AJ Friend <ajfriend@gmail.com>
2020-05-25 18:13:38 -0700

    Prepare for 3.6.1 alpha release (#119)
    * prepare for alpha release
    
    * playing with version tag events
    
    * setup PyPI push step
    
    * only upload on push
    
    * forgot the v!!
    
    * hmmm

--- src/h3/_version.py
+++ src/h3/_version.py
@@ -1,4 +1,4 @@
-__version__ = '3.6.1'
+__version__ = '3.6.1a1'
 __description__ = 'Hierarchical hexagonal geospatial indexing system'
 __url__ = 'https://github.com/uber/h3-py'
 __license__ = "Apache 2.0 License"
