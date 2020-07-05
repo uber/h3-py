eb893a195e1cdebfeb7d317515379c665e378bd6
AJ Friend <ajfriend@gmail.com>
2020-07-04 17:34:24 -0700

    retry CI

--- src/h3/unstable/__init__.py
+++ src/h3/unstable/__init__.py
@@ -1,7 +1,8 @@
 # flake8: noqa
 
 import warnings
-s = 'Modules under `h3.unstable` are experimental, and may change at any time.'
-warnings.warn(s)
+warnings.warn(
+    'Modules under `h3.unstable` are experimental, and may change at any time.'
+)
 
 from . import v4
