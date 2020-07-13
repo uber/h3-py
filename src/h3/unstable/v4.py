f01a6ddedcba42a72609175ebbc0cb32d2499db9
AJ Friend <ajfriend@gmail.com>
2020-07-13 16:19:10 -0700

    more traditional names

--- src/h3/unstable/v4.py
+++ src/h3/unstable/v4.py
@@ -1,57 +1,52 @@
 # flake8: noqa
 
-# todo: what are the pythonic names?
-
-# what if we put this in an object? what would it look like?
-# a temporary API wrapper
-
-# what if we organized by module
-
 from ..api.basic_str import (
+    # todo: implement is_valid_index
+
     compact as
-        compact,
+        compact_cells,
 
     edge_length as
-        hex_edge_length_avg,
+        get_hexagon_edge_length_avg,
 
     geo_to_h3 as
         point_to_cell,
 
     get_destination_h3_index_from_unidirectional_edge as
-        edge_destination,
+        get_directed_edge_destination,
 
     get_h3_indexes_from_unidirectional_edge as
-        directed_edge_cells,
+        directed_edge_to_cells,
 
     get_h3_unidirectional_edge as
         cells_to_directed_edge,
 
     get_h3_unidirectional_edge_boundary as
-        directed_edge_boundary, # what's a boundary?
+        directed_edge_to_boundary,
 
     get_h3_unidirectional_edges_from_hexagon as
         origin_to_directed_edges,
 
     get_origin_h3_index_from_unidirectional_edge as
-        edge_origin,
+        get_directed_edge_origin,
 
     get_pentagon_indexes as
-        pentagons,
+        get_pentagons,
 
     get_res0_indexes as
-        res0_cells,
+        get_res0_cells,
 
     h3_distance as
         grid_distance,
 
     h3_get_base_cell as
-        base_cell_number,
+        get_base_cell_number,
 
     h3_get_faces as
-        faces,
+        get_icosahedron_faces,
 
     h3_get_resolution as
-        resolution,
+        get_resolution,
 
     h3_indexes_are_neighbors as
         are_neighbor_cells,
@@ -61,7 +56,6 @@ from ..api.basic_str import (
 
     h3_is_res_class_III as
         is_res_class_III,
-    # h3_is_res_class_iii as _, # executive decision!
 
     h3_is_valid as
         is_valid_cell,
@@ -73,10 +67,10 @@ from ..api.basic_str import (
         cells_to_multipolygon,
 
     h3_to_center_child as
-        center_child,
+        cell_to_center_child,
 
     h3_to_children as
-        children,
+        cell_to_children,
 
     h3_to_geo as
         cell_to_point,
@@ -85,7 +79,7 @@ from ..api.basic_str import (
         cell_to_boundary,
 
     h3_to_parent as
-        parent,
+        cell_to_parent,
 
     h3_to_string as
         int_to_str,
@@ -94,7 +88,7 @@ from ..api.basic_str import (
         is_valid_directed_edge,
 
     hex_area as
-        hex_area_avg,
+        get_hexagon_area_avg,
 
     # hex_range as _,
     # hex_range_distances as _, # not sure we want this one; easy for user to implement
@@ -102,19 +96,15 @@ from ..api.basic_str import (
 
     hex_ring as
         grid_ring,
-    hex_ring as
-        ring, # used often enough that i think it deserves an alias
 
     k_ring as
         grid_disk,
-    k_ring as
-        disk, # used often enough that i think it deserves an alias
 
     k_ring_distances as
         grid_disk_distances,
 
     num_hexagons as
-        num_cells,
+        get_num_cells,
 
     polyfill as
         polygon_to_cells,
@@ -128,7 +118,7 @@ from ..api.basic_str import (
         str_to_int,
 
     uncompact as
-        uncompact,
+        uncompact_cells,
 
     versions as
         versions,
