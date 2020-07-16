6951ba42595067e1de5c88bfa1c76953820a52f2
AJ Friend <ajfriend@gmail.com>
2020-07-16 14:03:33 -0700

    clean things up a bit

--- src/h3/unstable/vect.py
+++ src/h3/unstable/vect.py
@@ -1,4 +1,23 @@
-from .._cy.unstable_vect import (
-    haversine_vect,
-    geo_to_h3_vect,
-)
+from .._cy import unstable_vect as _vect
+
+import numpy as np
+
+
+def geo_to_h3(lats, lngs, res):
+    assert len(lats) == len(lngs)
+
+    out = np.zeros(len(lats), dtype='uint64')
+
+    _vect.geo_to_h3_vect(lats, lngs, res, out)
+
+    return out
+
+
+def cell_haversine(a, b):
+    assert len(a) == len(b)
+
+    out = np.zeros(len(a), dtype='double')
+
+    _vect.haversine_vect(a, b, out)
+
+    return out
