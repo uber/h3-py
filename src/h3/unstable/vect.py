71aac1a76b4ecdc056857cfebde956d42359482f
AJ Friend <ajfriend@gmail.com>
2020-07-20 08:43:10 -0700

    add some docstrings

--- src/h3/unstable/vect.py
+++ src/h3/unstable/vect.py
@@ -4,6 +4,20 @@ import numpy as np
 
 
 def geo_to_h3(lats, lngs, res):
+    """
+    Convert arrays describing lat/lng paris to cells.
+
+    Parameters
+    ----------
+    lats, lngs : arrays of floats
+
+    res: int
+        Resolution for output cells.
+
+    Returns
+    -------
+    array of H3Cells
+    """
     assert len(lats) == len(lngs)
 
     out = np.zeros(len(lats), dtype='uint64')
@@ -14,6 +28,21 @@ def geo_to_h3(lats, lngs, res):
 
 
 def cell_haversine(a, b):
+    """
+    Compute haversine distance between the centers of cells given in
+    arrays a and b.
+
+
+    Parameters
+    ----------
+    a, b : arrays of H3Cell
+
+    Returns
+    -------
+    float
+        Haversine distance in kilometers
+    """
+
     assert len(a) == len(b)
 
     out = np.zeros(len(a), dtype='double')
