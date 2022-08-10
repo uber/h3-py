9e268cf3e792a6af0da2c928ffd0af6f1746782e
AJ Friend <ajfriend@gmail.com>
2022-08-10 16:35:19 -0400

    remove: from h3 import h3 (#270)

--- src/h3/__init__.py
+++ src/h3/__init__.py
@@ -3,10 +3,6 @@
 from .api.basic_str import *
 from ._version import __version__
 
-#todo: remove in 4.0; only here for backward compatibility
-from .api import basic_str as h3
-
-
 from ._cy import (
     UnknownH3ErrorCode,
     H3BaseException,
