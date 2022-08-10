70ae119fd25cc765b411f42bdedea23c56276e33
AJ Friend <ajfriend@gmail.com>
2022-08-09 17:40:22 -0700

    clearer logical flow

--- src/h3/_cy/memory.pyx
+++ src/h3/_cy/memory.pyx
@@ -112,9 +112,6 @@ cdef H3int[:] empty_memory_view():
 
 cdef _remove_zeros(H3MemoryManager x):
     x.n = move_nonzeros(x.ptr, x.n)
-    if x.n == 0:
-        h3_free(x.ptr)
-        x.ptr = NULL
 
     x.ptr = <H3int*> h3_realloc(x.ptr, x.n*sizeof(H3int))
     if not x.ptr:
@@ -129,15 +126,15 @@ cdef H3int[:] _create_mv(H3MemoryManager x):
         h3_free(x.ptr)
         x.ptr = NULL
         return empty_memory_view()
+    else:
+        mv = <H3int[:x.n]> x.ptr
+        mv.callback_free_data = h3_free
 
-    mv = <H3int[:x.n]> x.ptr
-    mv.callback_free_data = h3_free
+        # responsibility for the memory moves from this object to the array/memoryview
+        x.ptr = NULL
+        x.n = 0
 
-    # responsibility for the memory moves from this object to the array/memoryview
-    x.ptr = NULL
-    x.n = 0
-
-    return mv
+        return mv
 
 
 """
