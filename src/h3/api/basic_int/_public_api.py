0bc179fa0972899f48257c8eb89c8af8bd887efc
AJ Friend <ajfriend@gmail.com>
2022-08-14 14:24:01 -0700

    _binding to _b

--- src/h3/api/basic_int/_public_api.py
+++ src/h3/api/basic_int/_public_api.py
@@ -6,58 +6,58 @@ being able to understand the H3 API.
 
 This file exists to avoid dynamically modifying `globals` and support static tooling.
 """
-from ._binding import _binding
+from ._binding import _binding as _b
 
 
-is_valid_cell = _binding.is_valid_cell
-is_pentagon = _binding.is_pentagon
-is_valid_directed_edge = _binding.is_valid_directed_edge
-is_res_class_III = _binding.is_res_class_III
+is_valid_cell = _b.is_valid_cell
+is_pentagon = _b.is_pentagon
+is_valid_directed_edge = _b.is_valid_directed_edge
+is_res_class_III = _b.is_res_class_III
 
-int_to_string = _binding.int_to_string
-string_to_int = _binding.string_to_int
+int_to_string = _b.int_to_string
+string_to_int = _b.string_to_int
 
-cell_area = _binding.cell_area
-hex_area = _binding.hex_area
-edge_length = _binding.edge_length
-exact_edge_length = _binding.exact_edge_length
-great_circle_distance = _binding.great_circle_distance
+cell_area = _b.cell_area
+hex_area = _b.hex_area
+edge_length = _b.edge_length
+exact_edge_length = _b.exact_edge_length
+great_circle_distance = _b.great_circle_distance
 
-cell_to_local_ij = _binding.cell_to_local_ij
-local_ij_to_cell = _binding.local_ij_to_cell
-latlng_to_cell = _binding.latlng_to_cell
-cell_to_latlng = _binding.cell_to_latlng
-cell_to_boundary = _binding.cell_to_boundary
+cell_to_local_ij = _b.cell_to_local_ij
+local_ij_to_cell = _b.local_ij_to_cell
+latlng_to_cell = _b.latlng_to_cell
+cell_to_latlng = _b.cell_to_latlng
+cell_to_boundary = _b.cell_to_boundary
 
-grid_distance = _binding.grid_distance
-grid_ring = _binding.grid_ring
-grid_disk = _binding.grid_disk
-grid_path_cells = _binding.grid_path_cells
+grid_distance = _b.grid_distance
+grid_ring = _b.grid_ring
+grid_disk = _b.grid_disk
+grid_path_cells = _b.grid_path_cells
 
-get_pentagons = _binding.get_pentagons
-get_res0_cells = _binding.get_res0_cells
-get_num_cells = _binding.get_num_cells
-get_base_cell_number = _binding.get_base_cell_number
-get_icosahedron_faces = _binding.get_icosahedron_faces
-get_resolution = _binding.get_resolution
+get_pentagons = _b.get_pentagons
+get_res0_cells = _b.get_res0_cells
+get_num_cells = _b.get_num_cells
+get_base_cell_number = _b.get_base_cell_number
+get_icosahedron_faces = _b.get_icosahedron_faces
+get_resolution = _b.get_resolution
 
-cell_to_parent = _binding.cell_to_parent
-cell_to_children = _binding.cell_to_children
-cell_to_center_child = _binding.cell_to_center_child
-compact_cells = _binding.compact_cells
-uncompact_cells = _binding.uncompact_cells
+cell_to_parent = _b.cell_to_parent
+cell_to_children = _b.cell_to_children
+cell_to_center_child = _b.cell_to_center_child
+compact_cells = _b.compact_cells
+uncompact_cells = _b.uncompact_cells
 
-polyfill = _binding.polyfill
-polyfill_geojson = _binding.polyfill_geojson
-polyfill_polygon = _binding.polyfill_polygon
-cells_to_multi_polygon = _binding.cells_to_multi_polygon
+polyfill = _b.polyfill
+polyfill_geojson = _b.polyfill_geojson
+polyfill_polygon = _b.polyfill_polygon
+cells_to_multi_polygon = _b.cells_to_multi_polygon
 
-are_neighbor_cells = _binding.are_neighbor_cells
-get_directed_edge_origin = _binding.get_directed_edge_origin
-get_directed_edge_destination = _binding.get_directed_edge_destination
-directed_edge_to_cells = _binding.directed_edge_to_cells
-cells_to_directed_edge = _binding.cells_to_directed_edge
-directed_edge_to_boundary = _binding.directed_edge_to_boundary
-origin_to_directed_edges = _binding.origin_to_directed_edges
+are_neighbor_cells = _b.are_neighbor_cells
+get_directed_edge_origin = _b.get_directed_edge_origin
+get_directed_edge_destination = _b.get_directed_edge_destination
+directed_edge_to_cells = _b.directed_edge_to_cells
+cells_to_directed_edge = _b.cells_to_directed_edge
+directed_edge_to_boundary = _b.directed_edge_to_boundary
+origin_to_directed_edges = _b.origin_to_directed_edges
 
-versions = _binding.versions
+versions = _b.versions
