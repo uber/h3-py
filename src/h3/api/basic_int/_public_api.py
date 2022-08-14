583e3935f192b9a0e1e934783623a7ed04c5c475
AJ Friend <ajfriend@gmail.com>
2022-08-14 14:21:05 -0700

    more grouping

--- src/h3/api/basic_int/_public_api.py
+++ src/h3/api/basic_int/_public_api.py
@@ -21,6 +21,8 @@ cell_area = _binding.cell_area
 hex_area = _binding.hex_area
 edge_length = _binding.edge_length
 exact_edge_length = _binding.exact_edge_length
+great_circle_distance = _binding.great_circle_distance
+
 cell_to_local_ij = _binding.cell_to_local_ij
 local_ij_to_cell = _binding.local_ij_to_cell
 latlng_to_cell = _binding.latlng_to_cell
@@ -39,13 +41,12 @@ get_base_cell_number = _binding.get_base_cell_number
 get_icosahedron_faces = _binding.get_icosahedron_faces
 get_resolution = _binding.get_resolution
 
-cell_to_center_child = _binding.cell_to_center_child
-cell_to_children = _binding.cell_to_children
 cell_to_parent = _binding.cell_to_parent
+cell_to_children = _binding.cell_to_children
+cell_to_center_child = _binding.cell_to_center_child
 compact_cells = _binding.compact_cells
 uncompact_cells = _binding.uncompact_cells
 
-great_circle_distance = _binding.great_circle_distance
 polyfill = _binding.polyfill
 polyfill_geojson = _binding.polyfill_geojson
 polyfill_polygon = _binding.polyfill_polygon
