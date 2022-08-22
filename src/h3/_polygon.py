1183cba4a5061d651d540781561dc53ba832f001
AJ Friend <ajfriend@gmail.com>
2022-08-21 22:37:44 -0700

    Try out a __repr__ for h3.Polygon

--- src/h3/_polygon.py
+++ src/h3/_polygon.py
@@ -4,3 +4,11 @@ class Polygon:
     def __init__(self, outer, *holes):
         self.outer = outer
         self.holes = holes
+
+    def __repr__(self):
+        s = '<h3.Polygon |outer|={}, |holes|={}>'.format(
+            len(self.outer),
+            tuple(map(len, self.holes)),
+        )
+
+        return s
