0ae611507341b037f6c590962259e2c07304c442
AJ Friend <ajfriend@gmail.com>
2023-03-25 20:22:31 -0700

    Remove gj (#316)
    * Remove geojson logic from cell_to_boundary and directed_edge_to_boundary
    
    * fix a warning about return values in tests from pytest
    
    * Remove geo_json logic from cells_to_multi_polygon
    
    * remove some unused tests
    
    * add back in polygons_to_cells

--- src/h3/_cy/geo.pyx
+++ src/h3/_cy/geo.pyx
@@ -192,7 +192,7 @@ def polygons_to_cells(polygons, int res):
     return hmm.to_mv()
 
 
-def cell_to_boundary(H3int h, bool geo_json=False):
+def cell_to_boundary(H3int h):
     """Compose an array of geo-coordinates that outlines a hexagonal cell"""
     cdef:
         h3lib.CellBoundary gb
@@ -206,15 +206,10 @@ def cell_to_boundary(H3int h, bool geo_json=False):
         for i in range(gb.num_verts)
     )
 
-    if geo_json:
-        #lat/lng -> lng/lat and last point same as first
-        verts += (verts[0],)
-        verts = tuple(v[::-1] for v in verts)
-
     return verts
 
 
-def directed_edge_to_boundary(H3int edge, bool geo_json=False):
+def directed_edge_to_boundary(H3int edge):
     """ Returns the CellBoundary containing the coordinates of the edge
     """
     cdef:
@@ -229,11 +224,6 @@ def directed_edge_to_boundary(H3int edge, bool geo_json=False):
         coord2deg(gb.verts[i])
         for i in range(gb.num_verts)
     )
-
-    if geo_json:
-        #lat/lng -> lng/lat and last point same as first
-        verts += (verts[0],)
-        verts = tuple(v[::-1] for v in verts)
 
     return verts
 
