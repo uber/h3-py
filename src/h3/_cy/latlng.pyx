06f96e82cc3dcca336a7f4288383e09182edaa9a
Shiran Yuan <shiran.yuan@outlook.com>
2024-08-25 06:06:33 +0000

    Update latlng.pyx

--- src/h3/_cy/latlng.pyx
+++ src/h3/_cy/latlng.pyx
@@ -6,7 +6,6 @@ from h3lib cimport bool, H3int
 from .util cimport (
     check_cell,
     check_edge,
-    check_vertex,
     check_res,
     deg2coord,
     coord2deg
@@ -246,46 +245,3 @@ cpdef double great_circle_distance(
         raise ValueError('Unknown unit: {}'.format(unit))
 
     return d
-
-# todo: one might want to move the following to a separate vertex.pyx file
-
-cpdef H3int cell_to_vertex(H3int h, int vertex_num) except 1:
-    cdef:
-        H3int out
-
-    check_cell(h)
-
-    check_for_error(
-        h3lib.cellToVertex(h, vertex_num, &out)
-    )
-
-    return out
-
-cpdef H3int[:] cell_to_vertexes(H3int h):
-    cdef:
-        H3int out
-    
-    check_cell(h)
-
-    hmm = H3MemoryManager(6)
-    check_for_error(
-        h3lib.cellToVertexes(h, hmm.ptr)
-    )
-    mv = hmm.to_mv()
-
-    return mv
-
-cpdef (double, double) vertex_to_latlng(H3int v) except *:
-    cdef:
-        h3lib.LatLng c
-
-    check_vertex(v)
-
-    check_for_error(
-        h3lib.vertexToLatLng(v, &c)
-    )
-
-    return coord2deg(c)
-
-cpdef bool is_valid_vertex(H3int v):
-    return h3lib.isValidVertex(v) == 1
