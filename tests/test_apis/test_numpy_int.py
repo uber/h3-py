e40971ee3c1cf2894cbe01fa0cd266d8389c562b
Jongbin Jung <jungjongbin86@gmail.com>
2024-10-04 15:48:11 -0700

    Add failing test

--- tests/test_apis/test_numpy_int.py
+++ tests/test_apis/test_numpy_int.py
@@ -5,7 +5,10 @@ from .. import util as u
 
 
 def test1():
-    lat, lng = 37.7752702151959, -122.418307270836,
+    lat, lng = (
+        37.7752702151959,
+        -122.418307270836,
+    )
     assert h3.latlng_to_cell(lat, lng, 9) == 617700169958293503
 
 
@@ -31,3 +34,19 @@ def test_compact_cells():
     assert isinstance(cells, np.ndarray)
 
     assert h3.compact_cells(cells) == [h]
+
+
+def test_numpy_str():
+    expected = [
+        617700169957507071,
+        617700169957769215,
+        617700169958031359,
+        617700169958293503,
+        617700169961177087,
+        617700169964847103,
+        617700169965109247,
+    ]
+    cells = np.array([h3.int_to_str(h) for h in expected])
+    got = [h3.str_to_int(c) for c in cells]
+
+    assert expected == got
