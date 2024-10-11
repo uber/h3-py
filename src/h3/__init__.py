b4983d5170323f954953611cab1d1a44267eb7cd
Jongbin Jung <jungjongbin86@gmail.com>
2024-10-11 16:22:36 -0700

    Remove unnecessary flake8 pragma

--- src/h3/__init__.py
+++ src/h3/__init__.py
@@ -1,27 +1,24 @@
-# flake8: noqa
-
-from .api.basic_str import *
-from ._version import __version__
-
 from ._cy import (
-    UnknownH3ErrorCode,
     H3BaseException,
-    H3GridNavigationError,
-    H3MemoryError,
-    H3ValueError,
-    H3FailedError,
-    H3DomainError,
-    H3LatLngDomainError,
-    H3ResDomainError,
     H3CellInvalidError,
     H3DirEdgeInvalidError,
-    H3UndirEdgeInvalidError,
-    H3VertexInvalidError,
-    H3PentagonError,
+    H3DomainError,
     H3DuplicateInputError,
-    H3NotNeighborsError,
-    H3ResMismatchError,
+    H3FailedError,
+    H3GridNavigationError,
+    H3LatLngDomainError,
     H3MemoryAllocError,
     H3MemoryBoundsError,
+    H3MemoryError,
+    H3NotNeighborsError,
     H3OptionInvalidError,
+    H3PentagonError,
+    H3ResDomainError,
+    H3ResMismatchError,
+    H3UndirEdgeInvalidError,
+    H3ValueError,
+    H3VertexInvalidError,
+    UnknownH3ErrorCode,
 )
+from ._version import __version__
+from .api.basic_str import *  # noqa
--- src/h3/_cy/__init__.py
+++ src/h3/_cy/__init__.py
@@ -1,5 +1,3 @@
-# flake8: noqa
-
 """
 This module should serve as the interface between the C/Cython code and
 the Python code. That is, it is an internal API.
@@ -14,93 +12,86 @@ function interface and input conversion (string to int, for instance).
 """
 
 from .cells import (
-    is_valid_cell,
-    is_pentagon,
-    get_base_cell_number,
-    get_resolution,
-    cell_to_parent,
-    grid_distance,
-    grid_disk,
-    grid_ring,
-    cell_to_children_size,
-    cell_to_children,
-    cell_to_child_pos,
-    child_pos_to_cell,
-    compact_cells,
-    uncompact_cells,
-    get_num_cells,
     average_hexagon_area,
     cell_area,
-    grid_path_cells,
-    is_res_class_iii,
+    cell_to_center_child,
+    cell_to_child_pos,
+    cell_to_children,
+    cell_to_children_size,
+    cell_to_local_ij,
+    cell_to_parent,
+    child_pos_to_cell,
+    compact_cells,
+    get_base_cell_number,
+    get_icosahedron_faces,
+    get_num_cells,
     get_pentagons,
     get_res0_cells,
-    cell_to_center_child,
-    get_icosahedron_faces,
-    cell_to_local_ij,
+    get_resolution,
+    grid_disk,
+    grid_distance,
+    grid_path_cells,
+    grid_ring,
+    is_pentagon,
+    is_res_class_iii,
+    is_valid_cell,
     local_ij_to_cell,
+    uncompact_cells,
 )
-
 from .edges import (
     are_neighbor_cells,
-    cells_to_directed_edge,
-    is_valid_directed_edge,
-    get_directed_edge_origin,
-    get_directed_edge_destination,
-    directed_edge_to_cells,
-    origin_to_directed_edges,
     average_hexagon_edge_length,
+    cells_to_directed_edge,
+    directed_edge_to_cells,
     edge_length,
+    get_directed_edge_destination,
+    get_directed_edge_origin,
+    is_valid_directed_edge,
+    origin_to_directed_edges,
 )
-
+from .error_system import (
+    H3BaseException,
+    H3CellInvalidError,
+    H3DirEdgeInvalidError,
+    H3DomainError,
+    H3DuplicateInputError,
+    H3FailedError,
+    H3GridNavigationError,
+    H3LatLngDomainError,
+    H3MemoryAllocError,
+    H3MemoryBoundsError,
+    H3MemoryError,
+    H3NotNeighborsError,
+    H3OptionInvalidError,
+    H3PentagonError,
+    H3ResDomainError,
+    H3ResMismatchError,
+    H3UndirEdgeInvalidError,
+    H3ValueError,
+    H3VertexInvalidError,
+    UnknownH3ErrorCode,
+)
 from .latlng import (
-    latlng_to_cell,
-    cell_to_latlng,
-    polygon_to_cells,
-    polygons_to_cells,
     cell_to_boundary,
+    cell_to_latlng,
     directed_edge_to_boundary,
     great_circle_distance,
+    latlng_to_cell,
+    polygon_to_cells,
+    polygons_to_cells,
 )
-
-from .vertex import (
-    cell_to_vertex,
-    cell_to_vertexes,
-    vertex_to_latlng,
-    is_valid_vertex,
-)
-
-from .to_multipoly import cells_to_multi_polygon
-
-from .util import (
-    c_version,
-    str_to_int,
-    int_to_str,
-)
-
 from .memory import (
     iter_to_mv,
 )
-
-from .error_system import (
-    UnknownH3ErrorCode,
-    H3BaseException,
-    H3GridNavigationError,
-    H3MemoryError,
-    H3ValueError,
-    H3FailedError,
-    H3DomainError,
-    H3LatLngDomainError,
-    H3ResDomainError,
-    H3CellInvalidError,
-    H3DirEdgeInvalidError,
-    H3UndirEdgeInvalidError,
-    H3VertexInvalidError,
-    H3PentagonError,
-    H3DuplicateInputError,
-    H3NotNeighborsError,
-    H3ResMismatchError,
-    H3MemoryAllocError,
-    H3MemoryBoundsError,
-    H3OptionInvalidError,
+from .to_multipoly import cells_to_multi_polygon
+from .util import (
+    c_version,
+    int_to_str,
+    str_to_int,
+)
+from .vertex import (
+    cell_to_vertex,
+    cell_to_vertexes,
+    is_valid_vertex,
+    vertex_to_latlng,
 )
