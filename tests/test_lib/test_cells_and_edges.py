822320f30bf7f9381b5c00c4f7800b7a7b6581bb
dyrpsf <dyrpsf@gmail.com>
2026-08-02 10:05:10 +0530

    test: restore 0x prefix to match actual exception message

--- tests/test_lib/test_cells_and_edges.py
+++ tests/test_lib/test_cells_and_edges.py
@@ -131,7 +131,7 @@ def test_parent_err():
     except Exception as e:
         msg = str(e)
 
-    expected = f'Invalid parent resolution -1 for cell {h}.'
+    expected = f'Invalid parent resolution -1 for cell 0x{h}.'
 
     assert msg == expected
 
